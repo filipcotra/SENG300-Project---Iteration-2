9afba183ae24a4fb13ae71c4bd6245c2bba715f9
Christian Salvador <41243724+KittenInAMitten@users.noreply.github.com>
2023-03-21 17:48:02 -0600

    Created a BaggingAreaControllerTemplate

---
+++ com.autovend.software/src/com/autovend/software/BaggingAreaController.java
@@ -0,0 +1,79 @@
+package com.autovend.software;
+
+import com.autovend.devices.AbstractDevice;
+import com.autovend.devices.ElectronicScale;
+import com.autovend.devices.SelfCheckoutStation;
+import com.autovend.devices.observers.AbstractDeviceObserver;
+import com.autovend.devices.observers.ElectronicScaleObserver;
+
+public class BaggingAreaController implements ElectronicScaleObserver{
+	
+	SelfCheckoutStation station;
+	CustomerIO customerIO;
+	AttendantIO attendantIO;
+	PaymentControllerLogic paymentController;
+	
+	public BaggingAreaController(SelfCheckoutStation station, CustomerIO customerIO, AttendantIO attendantIO, PaymentControllerLogic paymentController) {
+		this.station = station;
+		this.customerIO = customerIO;
+		this.attendantIO = attendantIO;
+		this.paymentController = paymentController;
+	}
+	
+	/**
+	 * Helper function to block the system by disabling all devices besides the bagging area.
+	 */
+	private void blockSystem() {
+		this.station.printer.disable();
+		this.station.mainScanner.disable();
+		this.station.handheldScanner.disable();
+		this.station.billInput.disable();
+		this.station.billOutput.disable();
+		this.station.billStorage.disable();
+		this.station.billValidator.disable();
+	}
+	
+	/**
+	 * Helper function to unblock the system by enabling all devices besides the bagging area.
+	 */
+	private void unblockSystem() {
+		this.station.printer.enable();
+		this.station.mainScanner.enable();
+		this.station.handheldScanner.enable();
+		this.station.billInput.enable();
+		this.station.billOutput.enable();
+		this.station.billStorage.enable();
+		this.station.billValidator.enable();
+	}
+
+	@Override
+	public void reactToEnabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reactToDisabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reactToWeightChangedEvent(ElectronicScale scale, double weightInGrams) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reactToOverloadEvent(ElectronicScale scale) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reactToOutOfOverloadEvent(ElectronicScale scale) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
