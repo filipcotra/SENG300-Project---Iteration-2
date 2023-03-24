cc7960d5dc56a84c447a3ba94ea252bac0be3bc1
FCotra <81344727+FCotra@users.noreply.github.com>
2023-03-23 19:57:40 -0600

    Resolved testing inconsistencies.

--- com.autovend.software.test/src/com/autovend/software/test/PaymentWithCashTest.java
+++ com.autovend.software.test/src/com/autovend/software/test/PaymentWithCashTest.java
@@ -38,9 +38,11 @@ import com.autovend.Coin;
 import com.autovend.devices.AbstractDevice;
 import com.autovend.devices.BillDispenser;
 import com.autovend.devices.BillSlot;
+import com.autovend.devices.BillValidator;
 import com.autovend.devices.CoinDispenser;
 import com.autovend.devices.CoinSlot;
 import com.autovend.devices.CoinTray;
+import com.autovend.devices.CoinValidator;
 import com.autovend.devices.DisabledException;
 import com.autovend.devices.OverloadException;
 import com.autovend.devices.SelfCheckoutStation;
@@ -48,9 +50,11 @@ import com.autovend.devices.SimulationException;
 import com.autovend.devices.observers.AbstractDeviceObserver;
 import com.autovend.devices.observers.BillDispenserObserver;
 import com.autovend.devices.observers.BillSlotObserver;
+import com.autovend.devices.observers.BillValidatorObserver;
 import com.autovend.devices.observers.CoinDispenserObserver;
 import com.autovend.devices.observers.CoinSlotObserver;
 import com.autovend.devices.observers.CoinTrayObserver;
+import com.autovend.devices.observers.CoinValidatorObserver;
 import com.autovend.software.AttendantIO;
 import com.autovend.software.CustomerIO;
 import com.autovend.software.PaymentControllerLogic;
@@ -97,6 +101,10 @@ public class PaymentWithCashTest {
 	ByteArrayOutputStream baos;
 	PrintStream ps;
 	Boolean attendantSignalled;
+	Boolean coinFalseNegative;
+	Boolean billFalseNegative;
+
+/* ---------------------------------- Stubs ---------------------------------------------------*/
 	
 	class DispenserStub implements BillDispenserObserver {
 
@@ -202,6 +210,60 @@ public class PaymentWithCashTest {
 		
 	}
 	
+	class CoinValidatorStub implements CoinValidatorObserver{
+
+		@Override
+		public void reactToEnabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void reactToDisabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void reactToValidCoinDetectedEvent(CoinValidator validator, BigDecimal value) {
+			coinFalseNegative = false;
+			
+		}
+
+		@Override
+		public void reactToInvalidCoinDetectedEvent(CoinValidator validator) {
+			coinFalseNegative = true;
+			
+		}
+	}
+	
+	class BillValidatorStub implements BillValidatorObserver{
+
+		@Override
+		public void reactToEnabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void reactToDisabledEvent(AbstractDevice<? extends AbstractDeviceObserver> device) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void reactToValidBillDetectedEvent(BillValidator validator, Currency currency, int value) {
+			billFalseNegative = false;
+			
+		}
+
+		@Override
+		public void reactToInvalidBillDetectedEvent(BillValidator validator) {
+			billFalseNegative = true;
+			
+		}
+
+	}
 	
 	class MyCustomerIO implements CustomerIO {
 
@@ -380,6 +442,8 @@ public class PaymentWithCashTest {
 		}
 		
 	}
+
+/* ---------------------------------- SetUp ---------------------------------------------------*/
 	
 	@Before
 	public void setUp() {
@@ -453,8 +517,13 @@ public class PaymentWithCashTest {
 		receiptPrinterController = new PrintReceipt(selfCheckoutStation, selfCheckoutStation.printer, customer, attendant);
 		paymentController = new PaymentControllerLogic(selfCheckoutStation, customer, attendant, receiptPrinterController);
 		paymentController.setCartTotal(BigDecimal.ZERO);
-		
+		coinFalseNegative = true;
+		billFalseNegative = true;
+		billObserver = new MyBillSlotObserver();
+		selfCheckoutStation.billInput.register(billObserver);
 	}
+
+/* ---------------------------------- Teardown ------------------------------------------------*/
 	
 	// Teardown to set all objects to null or default values, and restore system.out to console
 	@After
@@ -491,6 +560,8 @@ public class PaymentWithCashTest {
 		paymentController = null;
 		
 	}
+
+/* ---------------------------------- Tests ---------------------------------------------------*/
 	
 	/* Test Case: Inserting an invalid bill denomination to the self-checkout machine
 	 * 
@@ -507,9 +578,7 @@ public class PaymentWithCashTest {
 	@Test
 	public void addInvalidBillDenom_Test() {
 		paymentController.setCartTotal(BigDecimal.valueOf(100.00));
-		billObserver = new MyBillSlotObserver();
 		try {
-			selfCheckoutStation.billInput.register(billObserver);
 			selfCheckoutStation.billInput.accept(billHundred);
 			assertEquals(selfCheckoutStation.billInput,billObserver.device);
 			assertEquals(BigDecimal.valueOf(100.00),paymentController.getCartTotal());
@@ -535,10 +604,8 @@ public class PaymentWithCashTest {
 	@Test
 	public void addInvalidBillCurr_Test() {
 		paymentController.setCartTotal(BigDecimal.valueOf(100.00));
-		billObserver = new MyBillSlotObserver();
 		Bill usdBillTwenty = new Bill (20, Currency.getInstance("USD"));
 		try {
-			selfCheckoutStation.billInput.register(billObserver);
 			selfCheckoutStation.billInput.accept(usdBillTwenty);
 			assertEquals(selfCheckoutStation.billInput,billObserver.device);
 			assertEquals(BigDecimal.valueOf(100.00),paymentController.getCartTotal());
@@ -611,10 +678,12 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void payBillLessThanTotal_Test() {
-		
 		paymentController.setCartTotal(BigDecimal.valueOf(100.00));
+		selfCheckoutStation.billValidator.register(new BillValidatorStub());
 		try {
-			selfCheckoutStation.billInput.accept(billTwenty);
+			while(billFalseNegative) {	
+				selfCheckoutStation.billInput.accept(billTwenty);
+			}
 		} catch (DisabledException e) {
 			fail("A Disabled Exception should not have been thrown");
 		} catch (OverloadException e) {
@@ -634,8 +703,11 @@ public class PaymentWithCashTest {
 	@Test
 	public void payCoinLessThanTotal_Test() {
 		paymentController.setCartTotal(BigDecimal.valueOf(100.00));
+		selfCheckoutStation.coinValidator.register(new CoinValidatorStub());
 		try {
-			selfCheckoutStation.coinSlot.accept(coinToonie);
+			while(coinFalseNegative) {	
+				selfCheckoutStation.coinSlot.accept(coinToonie);
+			}
 		} catch (DisabledException e) {
 			fail("A Disabled Exception should not have been thrown");
 		}
@@ -657,16 +729,22 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void payTwiceBills() {
-		
+		selfCheckoutStation.billValidator.register(new BillValidatorStub());
 		paymentController.setCartTotal(BigDecimal.valueOf(100.00));
 		try {
 			// The customer first inserts a twenty dollar bill into the bill slot.
-			selfCheckoutStation.billInput.accept(billTwenty);
+			while(billFalseNegative) {
+				selfCheckoutStation.billInput.accept(billTwenty);
+			}
 			assertTrue(BigDecimal.valueOf(80.00).compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("20.0",paymentController.getAmountPaid());
 			
+			billFalseNegative = true;
+			
 			// The customer then inserts a five dollar bill into the bill slot.
-			selfCheckoutStation.billInput.accept(billFive);
+			while(billFalseNegative) {
+				selfCheckoutStation.billInput.accept(billFive);
+			}
 			assertTrue(BigDecimal.valueOf(75.00).compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("25.0",paymentController.getAmountPaid());
 			
@@ -691,16 +769,22 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void payTwiceCoins() {
-		
+		selfCheckoutStation.coinValidator.register(new CoinValidatorStub());
 		paymentController.setCartTotal(BigDecimal.valueOf(100.00));
 		try {
 			// The customer first inserts a $1 coin (loonie) into the coin slot.
-			selfCheckoutStation.coinSlot.accept(coinLoonie);
+			while(coinFalseNegative) {
+				selfCheckoutStation.coinSlot.accept(coinLoonie);
+			}
 			assertTrue(BigDecimal.valueOf(99.00).compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("1.00",paymentController.getAmountPaid());
 			
+			coinFalseNegative = true;
+			
 			// The customer then inserts a $0.10 coin (dime) into the coin slot.
-			selfCheckoutStation.coinSlot.accept(coinDime);
+			while(coinFalseNegative) {
+				selfCheckoutStation.coinSlot.accept(coinDime);
+			}
 			assertTrue(BigDecimal.valueOf(98.90).compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("1.10",paymentController.getAmountPaid());
 			
@@ -723,16 +807,21 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void payTwiceCoinThenBill() {
-		
+		selfCheckoutStation.coinValidator.register(new CoinValidatorStub());
+		selfCheckoutStation.billValidator.register(new BillValidatorStub());
 		paymentController.setCartTotal(BigDecimal.valueOf(100.00));
 		try {
 			// The customer first inserts a $0.25 coin (quarter) into the coin slot.
-			selfCheckoutStation.coinSlot.accept(coinQuarter);
+			while(coinFalseNegative) {
+				selfCheckoutStation.coinSlot.accept(coinQuarter);
+			}
 			assertTrue(BigDecimal.valueOf(99.75).compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("0.25",paymentController.getAmountPaid());
 			
 			// The customer then inserts a 50 dollar bill into the bill slot.
-			selfCheckoutStation.billInput.accept(billFifty);
+			while(billFalseNegative) {
+				selfCheckoutStation.billInput.accept(billFifty);
+			}
 			assertTrue(BigDecimal.valueOf(49.75).compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("50.25",paymentController.getAmountPaid());
 			
@@ -757,16 +846,21 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void payTwiceBillThenCoin() {
-		
+		selfCheckoutStation.coinValidator.register(new CoinValidatorStub());
+		selfCheckoutStation.billValidator.register(new BillValidatorStub());
 		paymentController.setCartTotal(BigDecimal.valueOf(100.00));
 		try {
 			// The customer first inserts a ten dollar bill into the bill slot.
-			selfCheckoutStation.billInput.accept(billTen);
+			while(billFalseNegative) {
+				selfCheckoutStation.billInput.accept(billTen);
+			}
 			assertTrue(BigDecimal.valueOf(90).compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("10.0",paymentController.getAmountPaid());
 			
 			// The customer then inserts a $2 coin (toonie) into the coin slot.
-			selfCheckoutStation.coinSlot.accept(coinToonie);
+			while(coinFalseNegative) {
+				selfCheckoutStation.coinSlot.accept(coinToonie);
+			}
 			assertTrue(BigDecimal.valueOf(88).compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("12.00",paymentController.getAmountPaid());
 			
@@ -790,11 +884,13 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void payFullBillNoChange() {
-		
+		selfCheckoutStation.billValidator.register(new BillValidatorStub());
 		paymentController.setCartTotal(BigDecimal.valueOf(50.00));
 		try {
 			// The customer pays the full fifty dollars using a single fifty dollar bill.
-			selfCheckoutStation.billInput.accept(billFifty);
+			while(billFalseNegative) {
+				selfCheckoutStation.billInput.accept(billFifty);
+			}
 			assertTrue(BigDecimal.ZERO.compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("50.0",paymentController.getAmountPaid());
 			assertEquals("0.0",paymentController.getTotalChange());
@@ -819,11 +915,13 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void payFullCoinNoChange() {
-		
+		selfCheckoutStation.coinValidator.register(new CoinValidatorStub());
 		paymentController.setCartTotal(BigDecimal.valueOf(0.05));
 		try {
 			// The customer pays the full $0.05 using a single nickel.
-			selfCheckoutStation.coinSlot.accept(coinNickel);
+			while(coinFalseNegative) {
+				selfCheckoutStation.coinSlot.accept(coinNickel);
+			}
 			assertTrue(BigDecimal.ZERO.compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("0.05",paymentController.getAmountPaid());
 			assertEquals("0.00",paymentController.getTotalChange());
@@ -848,12 +946,14 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void payFullWithChange_Test(){
-		
+		selfCheckoutStation.billValidator.register(new BillValidatorStub());
 		paymentController.setCartTotal(BigDecimal.valueOf(10.00));
 		try {
 			// The customer pays the full fifty dollars using a single fifty dollar bill.
 			System.out.println("Payment: " + billFifty.getValue());
-			selfCheckoutStation.billInput.accept(billFifty);
+			while(billFalseNegative) {
+				selfCheckoutStation.billInput.accept(billFifty);
+			}
 			assertTrue(BigDecimal.ZERO.compareTo(paymentController.getCartTotal()) == 0);
 			assertEquals("50.0",paymentController.getAmountPaid());
 			assertEquals("40.0",paymentController.getTotalChange());
@@ -878,11 +978,14 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void totalChangeDueThirtyDollars_Test() throws DisabledException, OverloadException{
+		selfCheckoutStation.billValidator.register(new BillValidatorStub());
 		billObserverStub = new DispenserStub();
 		selfCheckoutStation.billDispensers.get(20).register(billObserverStub);
 		selfCheckoutStation.billDispensers.get(10).register(billObserverStub);
 		paymentController.setCartTotal(BigDecimal.valueOf(20.00));		
-		selfCheckoutStation.billInput.accept(billFifty);
+		while(billFalseNegative) {
+			selfCheckoutStation.billInput.accept(billFifty);
+		}
 		assertEquals("30.0",paymentController.getTotalChange());
 		assertEquals("0.0",""+paymentController.getChangeDue());
 		assertEquals("[10, 20]",ejectedBills.toString());
@@ -901,21 +1004,6 @@ public class PaymentWithCashTest {
 		assertTrue(BigDecimal.valueOf(40.00).compareTo(paymentController.getCartTotal()) == 0);
 	}
 	
-	/* Test Case: To see if the AttendantIO is being properly notified
-	 * when change is below the minimum denom of 5.
-	 * 
-	 * Description: Will pay $5 when cost is 3, causing change of 2
-	 * 
-	 * Expected Result: AttendantIO should be called.
-	 */
-	@Test
-	public void changeTooSmallAttendantIO_Test() throws OverloadException {
-		paymentController.setCartTotal(BigDecimal.valueOf(3.00));
-		selfCheckoutStation.billInput.accept(billFive);
-		String expected = "changeRemainsNoDenom Called: 2.0";
-		assertEquals(expected,baos.toString());
-	}
-	
 	/* Test Case: When the denom that should be emitted is empty, but this
 	 * is not the smallest denom.
 	 * 
@@ -927,46 +1015,21 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void emitEmptyNotSmallest_Test() throws OverloadException {
+		selfCheckoutStation.billValidator.register(new BillValidatorStub());
 		// Emptying billDispenser(20)
 		selfCheckoutStation.billDispensers.get(20).unload();
 		billObserverStub = new DispenserStub();
 		selfCheckoutStation.billDispensers.get(20).register(billObserverStub);
 		selfCheckoutStation.billDispensers.get(10).register(billObserverStub);
 		paymentController.setCartTotal(BigDecimal.valueOf(30.00));		
-		selfCheckoutStation.billInput.accept(billFifty);
+		while(billFalseNegative) {
+			selfCheckoutStation.billInput.accept(billFifty);
+		}
 		assertEquals("20.0",paymentController.getTotalChange());
 		assertEquals("0.0",""+paymentController.getChangeDue());
 		assertEquals("[10, 10]",ejectedBills.toString());
 	}
 	
-	/* Test Case: When the denom that should be emitted is empty, but this
-	 * is the smallest denom.
-	 * 
-	 * Description: Will pay $10 when the charge is $5, which should lead to 
-	 * $5 being emitted. However, it will be empty.
-	 * 
-	 * Expected: AttendantIO should be called and disabledException should
-	 * be thrown as the machine will be suspended.
-	 */
-	@Test
-	public void emitEmptySmallest_Test() throws OverloadException {
-		// Emptying billDispenser(20)
-		selfCheckoutStation.billDispensers.get(5).unload();
-		billObserverStub = new DispenserStub();
-		selfCheckoutStation.billDispensers.get(20).register(billObserverStub);
-		selfCheckoutStation.billDispensers.get(10).register(billObserverStub);
-		paymentController.setCartTotal(BigDecimal.valueOf(5.00));		
-		try{
-			selfCheckoutStation.billInput.accept(billTen);
-			fail();
-		}
-		catch(DisabledException e) {
-			assertEquals("5.0",paymentController.getTotalChange());
-			assertEquals("5.0",""+paymentController.getChangeDue());
-			String expected = "changeRemainsNoDenom Called: 5.0";
-			assertEquals(expected,baos.toString());
-		}
-	}
 	/* Test Case: The customer pays over the total cart amount by 5 dollars and the total change is dispensed. 
 	 * 
 	 * Description: The cart total is set at $45. $50 is paid in a single bill.So one 5 dollar bill is ejected for
@@ -982,10 +1045,13 @@ public class PaymentWithCashTest {
 	 */
 	@Test
 	public void totalChangeDueFiveDollars() throws DisabledException, OverloadException{
+		selfCheckoutStation.billValidator.register(new BillValidatorStub());
 		billObserverStub = new DispenserStub();
 		selfCheckoutStation.billDispensers.get(5).register(billObserverStub);
 		paymentController.setCartTotal(BigDecimal.valueOf(45.00));		
-		selfCheckoutStation.billInput.accept(billFifty);
+		while(billFalseNegative) {
+			selfCheckoutStation.billInput.accept(billFifty);
+		}
 		assertEquals("5.0",paymentController.getTotalChange());	
 		assertEquals("0.0",""+paymentController.getChangeDue());	
 		assertEquals("[5]",ejectedBills.toString());
