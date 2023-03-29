b912d216df6d8dda6c095c0a27cd19e5aa8749c7
Christian S <41243724+KittenInAMitten@users.noreply.github.com>
2023-03-29 12:51:39 -0600

    Re-introduced UCID comment in BaggingAreaController.java

--- com.autovend.software/src/com/autovend/software/BaggingAreaController.java
+++ com.autovend.software/src/com/autovend/software/BaggingAreaController.java
@@ -1,3 +1,17 @@
+/*
+  * Brian Tran (30064686)
+  * Filip Cotra (30086750)
+  * Arian Safari (30161346)
+  * Justin Clibbett (30128271)
+  * Umar Ahmed (30145076)
+  * Farbod Moghaddam (30115199)
+  * Abdul Alkareem Biderkab (30156693)
+  * Naheen Kabir (30142101)
+  * Khalen Drissi (30133707)
+  * Darren Roszell (30163669)
+  * Justin Yee (30113485)
+  * Christian Salvador (30089672)
+  */
 package com.autovend.software;
 
 import com.autovend.devices.AbstractDevice;
